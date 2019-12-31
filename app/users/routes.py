4013aab72eef700db656fb30dbb3db06bcb44a87
young <2932045582@qq.com>
2019-12-31 22:24:32 +0800

    complete project

---
+++ app/users/routes.py
@@ -0,0 +1,71 @@
+# -*- coding:utf-8 -*-
+"""
+    :author: young
+    :DATE: 2019/12/31 19:28
+"""
+from flask import url_for, redirect, render_template, flash, request
+from flask_login import current_user, login_user, logout_user, login_required
+
+from app import bcrypt, db
+from app.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from app.models import User
+from app.utils import save_picture
+from . import user
+
+
+@user.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('user.login'))
+    return render_template('register.html', title='注册', form=form)
+
+
+@user.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            # flash('Login successful.', 'message')
+            return redirect(next_page) if next_page else redirect(url_for('main.home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='登陆', form=form)
+
+
+@user.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('main.home'))
+
+
+@user.route("/account", methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('user.account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='账户信息',
+                           image_file=image_file, form=form)
